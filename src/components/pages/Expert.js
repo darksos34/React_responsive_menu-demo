abd76ca825b9166dd39202a70effe0ac7c5e8940
Jordy Coder <j.hamwijk34@gmail.com>
2023-01-08 15:18:56 +0100

    Fix card

--- src/components/pages/Expert.js
+++ src/components/pages/Expert.js
@@ -22,7 +22,8 @@ export default function Expert (){
                         </nav>
                     </div>
                 </div>
-            </div>       <div className="container-login">
+            </div>
+            <div className="container-login">
                 <div className="card card-container">
                     <div className="header-title">
                         <nav>
@@ -30,7 +31,8 @@ export default function Expert (){
                         </nav>
                     </div>
                 </div>
-            </div>       <div className="container-login">
+            </div>
+            <div className="container-login">
                 <div className="card card-container">
                     <div className="header-title">
                         <nav>
@@ -38,15 +40,35 @@ export default function Expert (){
                         </nav>
                     </div>
                 </div>
-            </div>       <div className="container-login">
+            </div>
+            <div className="container-login">
                 <div className="card card-container">
                     <div className="header-title">
                         <nav>
                             <Card   img="https://www.moneyunder30.com/wp-content/uploads/2021/10/shutterstock_1489795766.jpg" title="Homesweet"  description="Dit is een test card"/>
                         </nav>
                     </div>
+                </div>   <div className="card card-container">
+                    <div className="header-title">
+                        <nav>
+                            <Card   img="https://www.moneyunder30.com/wp-content/uploads/2021/10/shutterstock_1489795766.jpg" title="Homesweet"  description="Dit is een test card"/>
+                        </nav>
+                    </div>
+                </div>   <div className="card card-container">
+                    <div className="header-title">
+                        <nav>
+                            <Card   img="https://www.moneyunder30.com/wp-content/uploads/2021/10/shutterstock_1489795766.jpg" title="Homesweet"  description="Dit is een test card"/>
+                        </nav>
+                    </div>
+                </div>   <div className="card card-container">
+                    <div className="header-title">
+                        <nav>
+                            <Card   img="https://www.moneyunder30.com/wp-content/uploads/2021/10/shutterstock_1489795766.jpg" title="Homesweet"  description="Dit is een test card"/>
+                        </nav>
+                    </div>
                 </div>
-            </div>       <div className="container-login">
+            </div>
+            <div className="container-login">
                 <div className="card card-container">
                     <div className="header-title">
                         <nav>
