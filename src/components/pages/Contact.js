0bd236150fde65773b60c45990fc31a260ba1c76
Jordy Coder <j.hamwijk34@gmail.com>
2023-06-17 16:00:32 +0200

    Add Contact form Email, and pictures

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -15,6 +15,34 @@ export default function Contact (){
                 </div>
             </div>
             <div className="container-login">
+                {/*<div className="card card-container">*/}
+                <div className="header-title">
+                    {/*<div className='container'>*/}
+                    <div style={{ marginTop: '10%' }}>
+                        <form>
+                            <div className='form-group'>
+                                <label>Your Name</label>
+                                <input name='name' type='text' className='form-control'     placeholder='Enter Your Name' />
+                            </div>
+                            <div className='form-group'>
+                                <label>Email address</label>
+                                <input name='email' type='email' className='form-control' placeholder='Enter Your Email' />
+                            </div>
+                            <div className="form-group">
+                                <label>Message</label>
+
+                                <textarea name="message" className="form-control"    placeholder="Enter Your Message"></textarea>
+                            </div>
+                            <button type='submit' className='btn btn-primary'>
+                                Send Email
+                            </button>
+                        </form>
+                        {/*</div>*/}
+                        {/*</div>*/}
+                    </div>
+                </div>
+            </div>
+            <div className="container-login">
                 <div className="card card-container">
                     <div className="header-title">
                         <nav>
@@ -63,6 +91,7 @@ export default function Contact (){
                 </div>
             </div>
         </div>
+
             <div className="footer-topic">
 
             </div>
